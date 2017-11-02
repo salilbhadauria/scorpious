395e9a13ed4b9eda2a53f132b70f824b16c1cb6b
Emiliano Castagnari (a.k.a Torian) <ecastag@gmail.com>
2017-11-01 23:15:39 -0300

    DCDO-3: vpc module output

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -5,3 +5,16 @@ output "vpc_id" {
 output "vpc_igw" {
   value = "${aws_internet_gateway.igw.id}"
 }
+
+output "public_subnet_ids" {
+  value = [ "${aws_subnet.public.*.id}" ]
+}
+
+output "private_subnet_ids" {
+  value = [ "${aws_subnet.private.*.id}" ]
+}
+
+output "private_egress_subnet_ids" {
+  value = [ "${aws_subnet.private_egress.*.id}" ]
+}
+
