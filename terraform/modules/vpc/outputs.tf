dd3cbbf98547d79c15622c6596a8164ee5c3bd8e
Emiliano Castagnari (a.k.a Torian) <ecastag@gmail.com>
2017-11-02 21:25:36 -0300

    DCDO-3: Export VPC route table ids

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,20 +1,34 @@
+# vim: ts=4:sw=4:et:ft=hcl
+
 output "vpc_id" {
-  value = "${aws_vpc.vpc.id}"
+    value = "${aws_vpc.vpc.id}"
 }
 
 output "vpc_igw" {
-  value = "${aws_internet_gateway.igw.id}"
+    value = "${aws_internet_gateway.igw.id}"
 }
 
 output "public_subnet_ids" {
-  value = [ "${aws_subnet.public.*.id}" ]
+    value = [ "${aws_subnet.public.*.id}" ]
 }
 
 output "private_subnet_ids" {
-  value = [ "${aws_subnet.private.*.id}" ]
+    value = [ "${aws_subnet.private.*.id}" ]
 }
 
 output "private_egress_subnet_ids" {
-  value = [ "${aws_subnet.private_egress.*.id}" ]
+    value = [ "${aws_subnet.private_egress.*.id}" ]
+}
+
+output "route_table_public" {
+    value = "${aws_route_table.public.id}"
+}
+
+output "route_table_private" {
+    value = "${aws_route_table.private.id}"
+}
+
+output "route_table_private_egress" {
+    value = "${aws_route_table.private_egress.id}"
 }
 
