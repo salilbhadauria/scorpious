6b51dd24c218ced49d4e1c2a6c3971b2bed98e44
Emiliano Castagnari (a.k.a Torian) <ecastag@gmail.com>
2017-11-07 15:53:13 -0300

    DCDO-3: ELB module refactor
     * Fixed tabs (4 spaces)
     * Use only one listerner (for now)
     * Use `tag` map
     * Removed unnecesary `environment` var
     * `dns_records` is an array of just the subdomains
       `[ "master", "www" ]`. The domain  will be appended
       automatically based on the `dns_zone_id` parameter

--- terraform/modules/elb/variables.tf
+++ terraform/modules/elb/variables.tf
@@ -1,15 +1,20 @@
 variable "elb_name" {}
+
 variable "elb_is_internal" {
   description = "Determines if the ELB is internal or not"
   default = false
 }
+
 variable "elb_security_group" {}
+
 variable "subnets" {
   type = "list"
 }
+
 variable "backend_port" {
   description = "The port the service on the EC2 instances listens on"
 }
+
 variable "backend_protocol" {
  description = "The protocol the backend service speaks"
  // Possible options are
@@ -18,9 +23,11 @@ variable "backend_protocol" {
  // - tcp
  // - ssl (secure tcp)
 }
+
 variable "frontend_port" {
   description = "The port the service on the EC2 instances listens on"
 }
+
 variable "frontend_protocol" {
  description = "The protocol the backend service speaks"
  // Possible options are
@@ -29,33 +36,41 @@ variable "frontend_protocol" {
  // - tcp
  // - ssl (secure tcp)
 }
+
 variable "health_check_target" {
   description = "The URL the ELB should use for health checks"
   // This is primarily used with http or https backend protocols
   // The format is like `HTTPS:443/health`
 }
+
 variable "healthy_threshold" {
   default = "2"
 }
+
 variable "unhealthy_threshold" {
   default = "2"
 }
+
 variable "timeout" {
   default = "3"
 }
+
 variable "interval" {
   default = "10"
 }
+
 variable "cross_zone_load_balancing" {
   default = "true"
 }
-variable "environment" {}
+
 variable "idle_timeout" {
   default = "300"
 }
+
 variable "connection_draining" {
   default = "true"
 }
+
 variable "connection_draining_timeout" {
   default = "300"
 }
@@ -79,5 +94,12 @@ variable "dns_eval_target_health" {
 
 variable "dns_failover_type" {
   description = "Failover routing policy. Values: ''|PRIMARY|SECONDARY"
-  default = ""
+  default     = "PRIMARY"
 }
+
+variable "tags" {
+    description = "Resource tags"
+    type        = "map"
+    default     = {}
+}
+
