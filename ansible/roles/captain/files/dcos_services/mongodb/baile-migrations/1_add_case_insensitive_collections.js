5f3763e32d55af942258457eb02c20e7cdfe98b7
Anthony DiFrancesco <anthony.difrancesco@sentrana.com>
2018-02-12 11:43:07 -0500

    add new baile migration

--- ansible/roles/captain/files/dcos_services/mongodb/baile-migrations/1_add_case_insensitive_collections.js
+++ ansible/roles/captain/files/dcos_services/mongodb/baile-migrations/1_add_case_insensitive_collections.js
@@ -1,14 +1,31 @@
 exports.migrate = function(client, done) {
-    var db = client.db;
-    db.createCollection("datasets", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("flows", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("modelRefs", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("projects", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("replays", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("CVModels", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("CVPredictions", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("albums", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("predictions", { collation: { locale: 'en_US', strength: 2 } } )
-    db.createCollection("pictures", { collation: { locale: 'en_US', strength: 2 } } )
-    done();
+    const db = client.db;
+    Promise.all([
+        db.createCollection("datasets", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("flows", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("modelRefs", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("projects", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("replays", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("CVModels", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("CVPredictions", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("albums", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("predictions", { collation: { locale: 'en_US', strength: 2 } } ),
+        db.createCollection("pictures", { collation: { locale: 'en_US', strength: 2 } } )
+    ]).then(() => done()).catch((err) => done(err));
+};
+
+exports.rollback = function(client, done) {
+    const db = client.db;
+    Promise.all([
+        db.dropCollection("datasets"),
+        db.dropCollection("flows"),
+        db.dropCollection("modelRefs"),
+        db.dropCollection("projects"),
+        db.dropCollection("replays"),
+        db.dropCollection("CVModels"),
+        db.dropCollection("CVPredictions"),
+        db.dropCollection("albums"),
+        db.dropCollection("predictions"),
+        db.dropCollection("pictures")
+    ]).then(() => done()).catch((err) => done(err));
 };
