05e4d959b0fab5ea5d7e55d8c70c773b026c1894
Anthony DiFrancesco <anthony.difrancesco@sentrana.com>
2018-02-27 16:43:24 -0500

    add new rabbitmq jar and argo elastic index

--- ansible/roles/captain/files/dcos_services/elasticsearch/scripts/elasticsearch_init.sh
+++ ansible/roles/captain/files/dcos_services/elasticsearch/scripts/elasticsearch_init.sh
@@ -5,3 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo ssh -i /opt/private_key -o StrictHostKeyChecking=no -f -L 9200:coordinator.elastic.l4lb.thisdcos.directory:9200 deployer@"$DCOS_MASTER_PRIVATE_IP" sleep 10
 source "$DIR/build-index-jobs-dev.sh"
 source "$DIR/build-index-job-heartbeats-dev.sh"
+source "$DIR/build-index-config-settings-dev.sh"
