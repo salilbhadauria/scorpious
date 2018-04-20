b4d9cc64e3793d5adf37f40c7380383b3c52471e
Anthony DiFrancesco <anthony.difrancesco@sentrana.com>
2018-04-20 13:19:02 -0400

    small fixes

--- import_s3.sh
+++ import_s3.sh
@@ -16,7 +16,7 @@ AWS_DCOS_APPS_BUCKET=$(awk -F\" '/^dcos_apps_bucket/{print $2}'  "environments/$
 ./terraform.sh import $CONFIG s3_buckets aws_s3_bucket.dcos_stack_bucket $AWS_DCOS_STACK_BUCKET
 
 # import existing stack bucket
-./terraform.sh import $CONFIG s3_buckets aws_s3_bucket.dcos_apps_bucket $AWS_APPS_STACK_BUCKET
+./terraform.sh import $CONFIG s3_buckets aws_s3_bucket.dcos_apps_bucket $AWS_DCOS_APPS_BUCKET
 
 # refresh to obtain outputs
 ./terraform.sh refresh $CONFIG s3_buckets
